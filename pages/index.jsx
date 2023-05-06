54f2762a6ffffb6a0caa32acebb1484174c87824
Looming <wladislav101@gmail.com>
2023-05-06 18:10:10 +0300

    fix error

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,7 @@
-import Awards from '@/pages/awards'
-import Biography from '@/pages/biography'
-import Contacts from '@/pages/contacts'
-import Experience from '@/pages/experience'
-import MyPorftfolio from '@/pages/my-portfolio'
-import WhatIDo from '@/pages/what-i-do'
-
-export default function Homme() {
+export default function Home() {
 	return (
 		<>
-			<Biography />
-			<WhatIDo />
-			<Awards />
-			<Experience />
-			<MyPorftfolio />
-			<Contacts />
+			<h2>Nothing yet...</h2>
 		</>
 	)
 }
